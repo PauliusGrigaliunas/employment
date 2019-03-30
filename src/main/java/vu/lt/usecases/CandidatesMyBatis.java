f78050415a3d21f280ff10faca799aaf1e1dc0db
Paulius <paulius.grigaliunas.pg@gmail.com>
2019-03-30 23:18:55 +0200

    Revert "Revert "router""
    This reverts commit 8420797a3d15086a149737b17914d0ff7c311d0e.

---
+++ src/main/java/vu/lt/usecases/CandidatesMyBatis.java
@@ -0,0 +1,40 @@
+package vu.lt.usecases;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.mybatis.cdi.Transactional;
+import vu.lt.mybatis.dao.CandidateMapper;
+import vu.lt.persistence.mybatis.model.Candidate;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import java.util.List;
+
+
+@Model
+public class CandidatesMyBatis {
+    @Inject
+    private CandidateMapper candidateMapper;
+
+    @Getter
+    private List<vu.lt.persistence.mybatis.model.Candidate> allCandidates;
+
+    @Getter @Setter
+    private Candidate candidateToCreate = new Candidate();
+
+    @PostConstruct
+    public void init() {
+        this.loadAllCandidates();
+    }
+
+    private void loadAllCandidates() {
+        this.allCandidates = candidateMapper.selectAll();
+    }
+
+    @Transactional
+    public String createCandidate() {
+        candidateMapper.insert(candidateToCreate);
+        return "/myBatis/candidates?faces-redirect=true";
+    }
+}
