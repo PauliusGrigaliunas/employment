6da27862ef35fac0b546707b1c87e41be5a1570b
Paulius <paulius.grigaliunas.pg@gmail.com>
2019-05-03 16:18:30 +0300

    Add asynchronous

---
+++ src/main/java/vu/lt/asynchronous/CompB.java
@@ -0,0 +1,30 @@
+package vu.lt.asynchronous;
+
+
+import javax.ejb.AsyncResult;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.io.Serializable;
+import java.util.concurrent.Future;
+
+@ApplicationScoped
+public class CompB implements Serializable {
+
+    @Inject
+    //@RescueOrAsync            // Asinchroninis komponentas negali naudoti @RequestScoped konteksto
+    private EntityManager em; // Jei šis komponentas turi dirbti su DB per JPA
+
+    @Transactional(Transactional.TxType.REQUIRES_NEW)
+    // be šios anotacijos negalėsime gauti @RescueOrAsync EntityManager
+    public Future<String> asyncMethod() {
+        System.out.println("CompB starts working on a big task...");
+        try {
+            Thread.sleep(10000); // sleep for 10 seconds
+        } catch (InterruptedException e) {
+        }
+        System.out.println("CompB: big task completed.");
+        return new AsyncResult<>("BIG result from component B :)");
+    }
+}
